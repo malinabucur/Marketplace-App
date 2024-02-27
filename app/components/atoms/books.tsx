04cd2c621ce5ff0a11bf32ef4a52473b44b9a9b9
malinabucur <malinabucur13@gmail.com>
2024-02-27 15:14:53 +0200

    rename the logo icon into books icon

---
+++ app/components/atoms/books.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+export default function BooksIcon() {
+  return (
+    <div>
+      <Image src="/logo.png" alt="Logo Image" width={100} height={100} />
+    </div>
+  );
+}
--- app/components/atoms/logo.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image"
-export default function LogoIcon() {
-   return(
-    <div>
-      <Image src="/logo.png" alt="Logo Image" width={100} height={100}/>
-    </div>
-   )
-
-}
