5cc5213ea0670a1086c17c728b38813566c9113c
malinabucur <malinabucur13@gmail.com>
2024-02-29 15:22:44 +0200

    update search bar

--- app/components/molecules/searchBar.tsx
+++ app/components/molecules/searchBar.tsx
@@ -3,7 +3,7 @@ import SearchIcon from "../atoms/search";
 
 export default function SearchBar() {
   return (
-    <form className="flex items-center max-w-lg mx-auto">
+    <form className="flex items-center">
       <div className="flex justify-between text-black text-lg border-b-2 border-[#6B6B6B] m-3 px-2 w-full">
         <Input placeholder="Search..." />
         <button type="submit">
