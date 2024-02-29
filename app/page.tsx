3622a95dd721863c9d2ae5a4b8345947a93796c5
malinabucur <malinabucur13@gmail.com>
2024-02-29 15:23:28 +0200

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import SearchBar from "./components/molecules/searchBar";
 import Navbar from "./components/organisms/navbar";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main className="bg-white min-h-screen">
       <Navbar />
-      <div className="m-2 grid justify-center">
-        <SearchBar />
-      </div>
     </main>
   );
 }
