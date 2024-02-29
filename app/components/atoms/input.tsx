86b11da65e385c598b510e26fcef48dcf49079f9
malinabucur <malinabucur13@gmail.com>
2024-02-29 15:22:16 +0200

    add bg to input

--- app/components/atoms/input.tsx
+++ app/components/atoms/input.tsx
@@ -1,7 +1,7 @@
 export default function Input({ placeholder }: any) {
   return (
     <div>
-      <input type="text" id="default-search" placeholder={placeholder} className="focus:outline-none" />
+      <input type="text" id="default-search" placeholder={placeholder} className="focus:outline-none bg-[#EEE5E3]" />
     </div>
   );
 }
