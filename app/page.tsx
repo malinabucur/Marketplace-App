9dc9c1a2ad1fb753f6db8f2df0fa640173b9a55b
malinabucur <malinabucur13@gmail.com>
2024-02-29 11:17:09 +0200

    add search bar to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import SearchBar from "./components/molecules/searchBar";
 import Navbar from "./components/organisms/navbar";
 
 export default function Home() {
   return (
     <main className="bg-white min-h-screen">
       <Navbar />
+      <div className="m-2 grid justify-center">
+        <SearchBar />
+      </div>
     </main>
   );
 }
