fa6c1623fb0ffc7cde3db3f4edecc1d9f99731a5
malinabucur <malinabucur13@gmail.com>
2024-03-01 12:33:26 +0200

    create handleSearch nethod for the search bar

--- app/components/molecules/searchBar.tsx
+++ app/components/molecules/searchBar.tsx
@@ -1,15 +1,22 @@
+import { ChangeEvent } from "react";
 import Input from "../atoms/input";
 import SearchIcon from "../atoms/search";
 
-export default function SearchBar() {
+interface SearchBarProps {
+  handleSearch: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ handleSearch }) => {
   return (
     <form className="flex items-center">
       <div className="flex justify-between text-black text-lg border-b-2 border-[#6B6B6B] m-3 px-2 w-full">
-        <Input placeholder="Search..." />
+        <Input placeholder="Search..." onChange={handleSearch} />
         <button type="submit">
           <SearchIcon />
         </button>
       </div>
     </form>
   );
-}
+};
+
+export default SearchBar;
