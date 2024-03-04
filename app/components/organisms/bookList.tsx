27fab21638e6486aa525f1ed60c5e031e7a0b6a6
malinabucur <malinabucur13@gmail.com>
2024-03-04 14:03:48 +0200

    create bookList interface and update the component to retrieve a list of BookCards with my book data

---
+++ app/components/organisms/bookList.tsx
@@ -0,0 +1,17 @@
+import BookCards from "./bookCards";
+import React from "react";
+import { BookListProps } from "../interfaces/IBookListProps";
+
+const BookList: React.FC<BookListProps> = ({ books }) => {
+  return (
+    <div className="flex flex-wrap justify-center my-4">
+      {books.map((book, i: number) => {
+        const thumbnail = book.volumeInfo?.imageLinks?.thumbnail || "";
+
+        return <BookCards key={i} image={thumbnail} title={book.volumeInfo.title} authors={book.volumeInfo.authors} publishedDate={book.volumeInfo.publishedDate} />;
+      })}
+    </div>
+  );
+};
+
+export default BookList;
