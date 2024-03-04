60a3cd5885991ae16705d09c813cdd26a2ce8160
malinabucur <malinabucur13@gmail.com>
2024-03-04 14:04:13 +0200

    create navbar interface and update the navbar

--- app/components/organisms/navbar.tsx
+++ app/components/organisms/navbar.tsx
@@ -1,18 +1,18 @@
-import Image from "next/image";
+import React, { Dispatch, SetStateAction } from "react";
 import Header from "../molecules/header";
-import { Autour_One } from "next/font/google";
+import Image from "next/image";
+import { NavbarProps } from "../interfaces/INavbarProps";
 
-export default function Navbar() {
+const Navbar: React.FC<NavbarProps> = ({ updateBooks }) => {
   return (
     <div className="bg-[#EEE5E3] w-full h-96">
       <div className="px-4">
-        <Header />
+        <Header updateBooks={updateBooks} />
       </div>
       <div className="flex justify-between py-[-10px]">
         <div className="px-44 my-[-30px]">
           <Image src="/accent_nav.png" alt="Navbar Image Accent" width={300} height={100} priority />
         </div>
-
         <div className="text-black abril-fatface-regular py-20 pr-48">
           <p className="text-5xl font-bold">Welcome to</p>
           <p className="text-7xl font-bold ml-10">ChapterCharm </p>
@@ -20,4 +20,6 @@ export default function Navbar() {
       </div>
     </div>
   );
-}
+};
+
+export default Navbar;
