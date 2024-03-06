e3abecd5f0ca8d7691302fbd305b0629ab587901
malinabucur <malinabucur13@gmail.com>
2024-03-06 12:43:56 +0200

    update SearchBar component to clear the search field

--- app/components/molecules/searchBar.tsx
+++ app/components/molecules/searchBar.tsx
@@ -1,13 +1,29 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useState } from "react";
 import Input from "../atoms/input";
 import SearchIcon from "../atoms/search";
 import { SearchBarProps } from "../interfaces/ISearchBarProps";
 
-const SearchBar: React.FC<SearchBarProps> = ({ handleSearch, searchBook }) => {
+const SearchBar: React.FC<SearchBarProps> = ({ handleSearch, searchBook, searchField }) => {
+  const [localSearchField, setLocalSearchField] = useState(searchField);
+
+  const handleLocalSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setLocalSearchField(e.target.value);
+    handleSearch(e);
+  };
+
+  const handleLocalSearchBook = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await searchBook(e);
+
+    setTimeout(() => {
+      setLocalSearchField("");
+    }, 50);
+  };
+
   return (
-    <form className="flex items-center" onSubmit={searchBook} action="">
-      <div className="flex justify-between text-black text-lg border-b-2 border-[#6B6B6B] m-3 px-2 w-full">
-        <Input placeholder="Search..." onChange={handleSearch} />
+    <form className="flex items-center border-b-2 border-[#6B6B6B]" onSubmit={handleLocalSearchBook} action="">
+      <div className="flex justify-between items-center text-black text-lg pb-1 px-2 w-full">
+        <Input placeholder="Search..." value={localSearchField} onChange={handleLocalSearch} />
         <button type="submit">
           <SearchIcon />
         </button>
