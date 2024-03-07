dc72927b9c4628f8e6c0aa476ae97962b70c59e5
malinabucur <malinabucur13@gmail.com>
2024-03-07 10:47:02 +0200

    display HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,9 @@
-"use client";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
-import { Book } from "./components/interfaces/IBook";
-import Footer from "./components/molecules/footer";
-import Navbar from "./components/organisms/navbar";
-import { handleSearch } from "./components/services/bookService";
-import Pagination from "./components/organisms/pagination";
+import HomePage from "./components/pages/Home";
 
-const Home: React.FC = () => {
-  const [books, setBooks] = useState<Book[]>([]);
-
-  const updateBooks: Dispatch<SetStateAction<Book[]>> = (newBooks: Book[] | ((prevState: Book[]) => Book[])) => {
-    setBooks((prevState) => (typeof newBooks === "function" ? newBooks(prevState) : newBooks));
-  };
-
-  const searchBooks = async (searchField: string) => {
-    await handleSearch(searchField);
-  };
-
-  useEffect(() => {
-    const loadDefaultBooks = async () => {
-      const defaultBooks = await handleSearch("drama");
-      updateBooks(defaultBooks);
-    };
-
-    loadDefaultBooks();
-  }, []);
-
+const Home = () => {
   return (
     <>
-      <Navbar updateBooks={updateBooks} searchBooks={searchBooks} />
-      <main className="bg-white min-h-screen py-3">
-        <Pagination books={books} />
-      </main>
-      <Footer />
+      <HomePage />
     </>
   );
 };
