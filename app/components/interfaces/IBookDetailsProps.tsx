a20d9474da6d32d9c3dd88dffac8e6cfd4fd6d1c
malinabucur <malinabucur13@gmail.com>
2024-03-07 10:37:15 +0200

    create BookDetails component

---
+++ app/components/interfaces/IBookDetailsProps.tsx
@@ -0,0 +1,10 @@
+export interface BookDetailsProps {
+  image: string;
+  title: string;
+  authors: string;
+  publishedDate: string;
+  description: string;
+  pageCount: number;
+  categories: string;
+  language: string;
+}
