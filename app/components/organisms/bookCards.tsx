0ff584a472c8d6d0f6232ee537f327bbcf0e27c0
malinabucur <malinabucur13@gmail.com>
2024-03-11 10:26:00 +0200

    some style changes

--- app/components/organisms/bookCards.tsx
+++ app/components/organisms/bookCards.tsx
@@ -29,11 +29,11 @@ const BookCards: React.FC<BookCardProps> = ({ id, image, title, authors, publish
   return (
     <>
       <button onClick={handleClick} type="button" data-twe-toggle="BookModal" data-twe-target="#BookModal" data-twe-ripple-init>
-        <div className="flex flex-col bg-white rounded-lg shadow-xl border-2 border-[#D3D3D3] w-60 h-auto mx-12 mb-6 ">
+        <div className="flex flex-col bg-white rounded-lg shadow-xl border-2 border-[#D3D3D3] w-60 h-[30rem] mx-12 mb-6 ">
           <div className="h-[22rem] overflow-hidden ">
             <Image src={image} alt="Book Cover Image" width={240} height={100} />
           </div>
-          <div className="flex-grow text-black text-base ml-1">
+          <div className="text-start text-black text-base ml-1">
             <p className="font-semibold">{title || "-"}</p>
             <p>{authors || "-"} </p>
             <p>{publishedDate || "-"}</p>
