f974cf03293ff0c649c8a6ed633ebf41c5454d8d
malinabucur <malinabucur13@gmail.com>
2024-03-14 13:21:35 +0200

    add SaleInfo properties and modify authors property type

--- app/components/interfaces/IBook.tsx
+++ app/components/interfaces/IBook.tsx
@@ -1,9 +1,13 @@
+import { SaleInfo } from "./ISaleInfo";
 import { VolumeInfo } from "./IVolumeInfo";
 
 export interface Book {
   id: string;
   volumeInfo: VolumeInfo;
   title: string;
-  authors: string;
+  authors: string | string[];
   publishedDate: string;
+  saleInfo: SaleInfo;
+  amount: string;
+  currencyCode: string;
 }
