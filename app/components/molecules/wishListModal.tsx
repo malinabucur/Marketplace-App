279d81721d294afc8dd3168878093fe1b044495c
malinabucur <malinabucur13@gmail.com>
2024-03-16 14:52:30 +0200

    some style changes

--- app/components/molecules/wishListModal.tsx
+++ app/components/molecules/wishListModal.tsx
@@ -1,6 +1,9 @@
 import React, { ReactNode, useEffect } from "react";
 
-const WishListModal: React.FC<{ onClose: () => void; children?: ReactNode; wishList: string[]; onBookClick: (title: string) => void }> = ({ onClose, children, wishList, onBookClick }) => {
+const WishListModal: React.FC<{ onClose: () => void; children?: ReactNode; wishList: { title: string; authors: string | string[]; image: string }[]; onBookClick: (title: string) => void }> = ({
+  onClose,
+  children,
+}) => {
   useEffect(() => {
     document.body.style.overflow = "hidden";
 
@@ -20,14 +23,14 @@ const WishListModal: React.FC<{ onClose: () => void; children?: ReactNode; wishL
   }, [onClose]);
 
   return (
-    <div className="relative z-10" aria-labelledby="WishListModal" role="dialog" aria-modal="true">
+    <div className="relative z-10" aria-labelledby="CartModal" role="dialog" aria-modal="true">
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center items-center">
           <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all ">
-            <div className="bg-white pl-10 py-5 w-[40rem]">
-              <div className="mt-3 text-left h-60">
+            <div className="bg-white pl-10 py-5  w-[45rem] overflow-scroll">
+              <div className="mt-3 text-left h-96">
                 <h3 className="text-2xl font-semibold leading-6 text-gray-900">Wish List</h3>
                 <div className="mt-4">
                   <p className="text-base text-black">{children}</p>
@@ -35,10 +38,8 @@ const WishListModal: React.FC<{ onClose: () => void; children?: ReactNode; wishL
               </div>
             </div>
             <div className="bg-gray-50 px-4 py-3">
-              <button
-                type="button"
-                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                Cancel
+              <button type="button" className="mt-3 inline-flex w-full justify-end rounded-md bg-white px-3 py-2 text-sm font-semibold text-black shadow-sm hover:text-red-500">
+                Close
               </button>
             </div>
           </div>
